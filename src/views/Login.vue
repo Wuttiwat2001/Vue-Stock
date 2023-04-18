1ebe222a668d8344567b402ca4b6d3c673a32735
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-18 18:54:46 +0700

    Link Register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,13 +2,25 @@
   <v-container>
     <v-row class="justify-center">
       <v-card max-width="400">
-        <v-img src="@/assets/vue_bg.jpg" height="200" />
+        <v-img
+          class="white--text align-end"
+          src="@/assets/vue_bg.jpg"
+          height="200"
+        >
+          <v-card-title primary-title> LOGIN </v-card-title>
+        </v-img>
         <v-card-text>
           <v-form>
             <!-- Username -->
-            <v-text-field name="username" label="Username" id="username" />
+            <v-text-field
+              color="success"
+              name="username"
+              label="Username"
+              id="username"
+            />
             <!-- Password -->
             <v-text-field
+              color="success"
               name="password"
               label="Password"
               id="password"
@@ -17,8 +29,8 @@
               @click:append="isShowPassword = !isShowPassword"
               counter
             />
-            <v-row class="justify-space-between py-3">
-              <v-btn text>Register</v-btn>
+            <v-row class="justify-space-between px-3 py-5">
+              <v-btn text @click.prevent="onClickRegister">Register</v-btn>
               <v-btn color="success">Login</v-btn>
             </v-row>
           </v-form>
@@ -28,12 +40,18 @@
   </v-container>
 </template>
 <script>
+import router from "@/router";
 export default {
   name: "Login",
   data() {
     return {
       isShowPassword: false,
     };
+  },
+  methods: {
+    onClickRegister() {
+      router.push("/register");
+    },
   },
 };
 </script>
