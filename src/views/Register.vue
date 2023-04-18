72c132552cca58ee4437cf09ed54bbf9ae4dfa70
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-18 19:44:44 +0700

    Register Layout

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,52 @@
 <template>
-<h1>Register</h1>
+  <v-container>
+    <v-row class="justify-center">
+      <v-card max-width="400">
+        <v-img
+          class="white--text align-end"
+          src="@/assets/vue_bg.jpg"
+          height="200"
+        >
+          <v-card-title primary-title> REGISTER </v-card-title>
+        </v-img>
+        <v-card-text>
+          <v-form>
+            <!-- Username -->
+            <v-text-field
+              color="success"
+              name="username"
+              label="Username"
+              id="username"
+            />
+            <!-- Password -->
+            <v-text-field
+              color="success"
+              name="password"
+              label="Password"
+              id="password"
+              :type="isShowPassword ? 'text' : 'password'"
+              :append-icon="isShowPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="isShowPassword = !isShowPassword"
+              counter
+            />
+            <v-row class="justify-space-between px-3 py-5">
+              <v-btn text @click.prevent="$router.back()">Cancel</v-btn>
+              <v-btn color="success">Confirm</v-btn>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-  name: 'register'
-}
+  name: "Register",
+  data() {
+    return {
+      isShowPassword: false,
+    };
+  },
+};
 </script>
-<style>
-  
-</style>
+<style></style>
