e07b7c11606fcccc9887a1e65666b93ad817386e
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-18 18:42:07 +0700

    Login layout and Password Visibility

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,40 @@
 <template>
-<v-container>
-  <v-row class="justify-center">
-    <v-card max-width="400">
-      <v-img src="@/assets/vue_bg.jpg" height="200"/>
-      <v-card-text>
-        <v-form>
-          <!-- Username -->
-          <v-text-field
-            name="username"
-            label="Username"
-            id="username"
-          />
-          <!-- Password -->
-          <v-text-field
-            name="password"
-            label="Password"
-            id="password"
-            type="password"
-          />
-          <v-row class="justify-space-between py-3">
-            <v-btn text>Register</v-btn>
-            <v-btn color="success">Login</v-btn>
-          </v-row>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </v-row>
-</v-container>
+  <v-container>
+    <v-row class="justify-center">
+      <v-card max-width="400">
+        <v-img src="@/assets/vue_bg.jpg" height="200" />
+        <v-card-text>
+          <v-form>
+            <!-- Username -->
+            <v-text-field name="username" label="Username" id="username" />
+            <!-- Password -->
+            <v-text-field
+              name="password"
+              label="Password"
+              id="password"
+              :type="isShowPassword ? 'text' : 'password'"
+              :append-icon="isShowPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="isShowPassword = !isShowPassword"
+              counter
+            />
+            <v-row class="justify-space-between py-3">
+              <v-btn text>Register</v-btn>
+              <v-btn color="success">Login</v-btn>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-  name: 'login'
-}
+  name: "Login",
+  data() {
+    return {
+      isShowPassword: false,
+    };
+  },
+};
 </script>
-<style>
-  
-</style>
+<style></style>
