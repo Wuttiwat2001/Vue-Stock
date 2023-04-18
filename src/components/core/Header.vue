ed205218b252b9ae00e7672e7a209e800efec0e5
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-18 17:58:24 +0700

    Setup Header and Menu

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>Header</h1>
+  <div>
+    <v-app-bar
+      app
+      color="#1c2835"
+      dark
+    >
+      <v-toolbar-title>Stock {{ version }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <span>Username</span>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
 </template>
 <script>
 export default {
-  name: 'header'
+  name: 'Header',
+  computed:{
+    version(){
+      return process.env.VUE_APP_VERSION
+    }
+  }
 }
 </script>
 <style>
--- src/components/core/Menu.vue
+++ src/components/core/Menu.vue
@@ -1,11 +1,55 @@
 <template>
-  <h1>Menu</h1>
+  <v-navigation-drawer app permanent absolute>
+    <router-link to="/" exact>
+      <div class="bg-div">
+        <v-img src="@/assets/stock_logo.png" width="100%" />
+      </div>
+    </router-link>
+
+    <v-divider></v-divider>
+
+    <v-list shaped>
+      <v-subheader>MENUS</v-subheader>
+      <v-list-item-group v-model="selectedMenu" mandatory color="primary">
+        <v-list-item v-for="([icon,text,route],index) in menus" :key="index" link @click="onClickMenu(route)"> 
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 <script>
+import router from '@/router';
 export default {
-  name: 'menu'
-}
+  name: "menu",
+  data() {
+    return {
+      selectedMenu: 0,
+      menus: [
+        ["mdi-apps-box", "Stock", "/stock"],
+        ["mdi-chart-areaspline", "Report", "/report"],
+        ["mdi-file-document-check-outline", "About", "/about"],
+      ],
+    };
+  },
+  methods:{
+    onClickMenu(link){
+      router.push(link).catch(err => {
+        alert(err)
+      })
+    }
+  }
+};
 </script>
-<style>
-  
+<style scoped>
+.bg-div {
+  background-color: #4caf50;
+}
 </style>
