db6af4922c9ed7ddb79f38c5eda2319e76d17576
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-18 18:34:47 +0700

    Login Layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,31 @@
 <template>
 <v-container>
-  <h1>Login</h1>
+  <v-row class="justify-center">
+    <v-card max-width="400">
+      <v-img src="@/assets/vue_bg.jpg" height="200"/>
+      <v-card-text>
+        <v-form>
+          <!-- Username -->
+          <v-text-field
+            name="username"
+            label="Username"
+            id="username"
+          />
+          <!-- Password -->
+          <v-text-field
+            name="password"
+            label="Password"
+            id="password"
+            type="password"
+          />
+          <v-row class="justify-space-between py-3">
+            <v-btn text>Register</v-btn>
+            <v-btn color="success">Login</v-btn>
+          </v-row>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-row>
 </v-container>
 </template>
 <script>
