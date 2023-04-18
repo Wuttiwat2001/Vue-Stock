87539255b1f8eefac11f4edaaf2f9bb5ed79d38f
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-19 02:41:22 +0700

    Route Define and Separate Router

--- backend_dummy/server.js
+++ backend_dummy/server.js
@@ -1,1 +1,11 @@
-console.log("Wuttiwat")
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/upload"))
+
+
+
+app.use("/api/v2", require("./api"))
+
+app.listen(3000, () => {
+  console.log("Server is running..");
+});
